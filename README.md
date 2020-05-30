65104042f4ed4f923a53c4e4df0fdc4b66a926c4
Deepak Raj Purtushothaman <deepakrajpurushothaman@gmail.com>
2020-05-30 08:22:56 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+Personal website built using GitHub Pages, HTML5/CSS3, Bootstrap 4, Start Bootstrap - Landing Page Template, and further resources. Check it out at bharathjaladi.com!
---
+++ accordion.js
@@ -0,0 +1,178 @@
+$('.panel-clr7').toggleClass('on', true);
+
+$(function () {
+    $('.panel-clr').click(function () {
+      $('.panel-clr').toggleClass('on');
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+  $(function () {
+    $('.panel-clr1').click(function () {
+      $('.panel-clr1').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+  $(function () {
+    $('.panel-clr2').click(function () {
+      $('.panel-clr2').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+  $(function () {
+    $('.panel-clr3').click(function () {
+      $('.panel-clr3').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+
+  $(function () {
+    $('.panel-clr4').click(function () {
+      $('.panel-clr4').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+    $(function () {
+    $('.panel-clr5').click(function () {
+      $('.panel-clr5').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+    $(function () {
+    $('.panel-clr6').click(function () {
+      $('.panel-clr6').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr7').toggleClass('on', false);
+    })
+  });
+
+    $(function () {
+    $('.panel-clr7').click(function () {
+      $('.panel-clr7').toggleClass('on');
+      $('.panel-clr').toggleClass('on', false);
+      $('.panel-clr1').toggleClass('on', false);
+      $('.panel-clr2').toggleClass('on', false);
+      $('.panel-clr3').toggleClass('on', false);
+      $('.panel-clr4').toggleClass('on', false);
+      $('.panel-clr5').toggleClass('on', false);
+      $('.panel-clr6').toggleClass('on', false);
+    })
+  });
+
+  $(function () {
+    $('#headingOne').hover(function () {
+      $('#headingOne').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingOne').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingTwo').hover(function () {
+      $('#headingTwo').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingTwo').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingThree').hover(function () {
+      $('#headingThree').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingThree').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingFour').hover(function () {
+      $('#headingFour').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingFour').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingFive').hover(function () {
+      $('#headingFive').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingFive').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingSix').hover(function () {
+      $('#headingSix').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingSix').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingSeven').hover(function () {
+      $('#headingSeven').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingSeven').css('background-color', '');
+    });
+  });
+
+  $(function () {
+    $('#headingEight').hover(function () {
+      $('#headingEight').css('background-color', '#e7e7e7');
+    }, function () {
+      // on mouseout, reset the background colour
+      $('#headingEight').css('background-color', '');
+    });
+  });
